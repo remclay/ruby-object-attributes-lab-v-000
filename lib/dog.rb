7eb85ecb7a3020cb63162541836df9379b411ecb
Remmy Clay <remmy.lauren@gmail.com>
2017-11-22 05:16:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,21 +15,3 @@ class Dog
     @breed
   end
 end
-
-class Person
-  def name=(name)
-    @name = (name)
-  end
-
-  def name
-    @name
-  end
-
-  def job=(job)
-    @job = job
-  end
-
-  def job
-    @job
-  end
-end
